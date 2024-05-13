8e660c35e0b3548242cdadf1f5645c0feb6843fa
Shailendra Bhargava <shailendrabhargavaa@gmail.com>
2024-05-13 17:40:23 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
-import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule],
   declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule,HttpClientModule, ReactiveFormsModule],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
