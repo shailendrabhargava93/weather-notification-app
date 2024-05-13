ecf0b30b669697c6b3996e17881d786b201f2c1d
Shailendra Bhargava <shailendrabhargavaa@gmail.com>
2024-05-13 18:38:06 +0530

    Update api.service.ts

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -14,34 +14,4 @@ export class ApiService {
     //localStorage.setItem('place', place);
     return this.httpClient.get(this.SERVER_URL + '&q=' + place + '&aqi=yes');
   }
-
-  public sendNotification() {
-    let place = localStorage.getItem('place');
-    let subs = localStorage.getItem('subscribed');
-    if (place && subs == 'true') {
-       this.httpClient
-        .get(this.SERVER_URL + '&q=' + place + '&aqi=yes')
-        .subscribe((data: any) => {
-          let weather = data.current.condition;
-          let location = data.location.name + ', ' + data.location.region;
-          if (Notification.permission === 'granted') {
-            //alert("we have permission");
-            const notification = new Notification('Weather Notification', {
-              body: weather.text + ' in ' + location,
-              icon: weather.icon,
-              badge: weather.icon,
-            });
-          } else if (Notification.permission !== 'denied') {
-            Notification.requestPermission().then((permission) => {
-              console.log(permission);
-              const notification = new Notification('Weather Notification', {
-                body: weather.text + ' in ' + location,
-                icon: weather.icon,
-                badge: weather.icon,
-              });
-            });
-          }
-        });
-    } else console.error('No place provided');
-  }
 }
