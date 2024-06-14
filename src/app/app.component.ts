cb09f18205e29f813c1795cf0dfe54724f3a3c90
Shailendra Bhargava <shailendrabhargavaa@gmail.com>
2024-06-14 18:58:23 +0530

    A2hS button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, HostListener } from "@angular/core";
 import { interval, Subscription } from "rxjs";
 import { ApiService } from "./api.service";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
@@ -161,5 +161,34 @@ export class AppComponent {
 
   onUnitChange() {
     this.isCelsius = !this.isCelsius;
+  }
+
+  deferredPrompt: any;
+  showButton = false;
+
+  @HostListener("window:beforeinstallprompt", ["$event"])
+  onbeforeinstallprompt(e: Event) {
+    console.log(e);
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    this.deferredPrompt = e;
+    this.showButton = true;
+  }
+
+  addToHomeScreen() {
+    // hide our user interface that shows our A2HS button
+    this.showButton = false;
+    // Show the prompt
+    this.deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    this.deferredPrompt.userChoice.then((choiceResult: { outcome: string }) => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the A2HS prompt");
+      } else {
+        this.toast.error("User dismissed the A2HS prompt");
+      }
+      this.deferredPrompt = null;
+    });
   }
 }
